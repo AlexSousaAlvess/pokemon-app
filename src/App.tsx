66877f31b0cef4f2f22d89450166137e82b165fc
AlexSousaAlvess <alexsousaalves317@gmail.com>
2021-08-20 00:12:12 -0300

    request poke-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import api from "./service/api";
+import Spin from "./assets/Spin.svg";
+
+import "./App.css";
 
 function App() {
+  const [pokemons, setPokemons] = useState<Array<any>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    api
+      .get("pokemon")
+      .then((res) => {
+        return res.data.results;
+      })
+      .then((results) => {
+        return Promise.all(results.map((res: any) => axios.get(res.url)));
+      })
+      .then((results) => {
+        setPokemons(results.map((res: any) => res.data));
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {pokemons.length ? (
+        pokemons.map((pokemon) => (
+          <div className="container">
+            <div className="card">
+              <div className="info">
+                <h3>{pokemon.name}</h3>
+                <div className="images">
+                  <img
+                    src={pokemon.sprites["front_default"]}
+                    alt={pokemon.name}
+                  />
+                  <img
+                    src={pokemon.sprites["back_default"]}
+                    alt={pokemon.name}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <img src={Spin} alt={"loading"} className="loading" />
+      )}
+    </>
   );
 }
 
